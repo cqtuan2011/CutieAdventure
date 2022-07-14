8b5a64dc0f5a37abb1181f9508a437c3e201c199
cqtuan <tuancq2011@gmail.com>
2022-07-14 16:37:22 +0700

    Animate Button

---
+++ Assets/Scripts/UI_BottomUp.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class UI_BottomUp : MonoBehaviour
+{
+    [SerializeField] private List<RectTransform> buttons;
+
+    [SerializeField] private float delayStart = 0.5f;
+    [SerializeField] private float delayDisplay = 0.1f;
+
+    private void Start()
+    {
+        StartCoroutine(BottomUpAnimation(delayStart, delayDisplay));
+    }
+
+    IEnumerator BottomUpAnimation(float delayStart, float delayDisplay)
+    {
+        yield return new WaitForSeconds(delayStart);
+
+        for (int i = 0; i < buttons.Count; i++)
+        {
+            buttons[i].DOAnchorPosY(-85f, 0.6f).SetEase(Ease.OutBack);
+            yield return new WaitForSeconds(delayDisplay);
+        }
+    }
+}
